97beeae814dcac85874e9c194cf8a8f4fa8863fb
Andrew Erickson <aerickson@mozilla.com>
2019-08-28 17:17:31 -0700

    run formatter

--- test/test_tq.py
+++ test/test_tq.py
@@ -7,23 +7,26 @@ import pytest
 @pytest.fixture
 # script working directory
 def root_dir():
-  swd = os.path.dirname(os.path.abspath(__file__))
-  return os.path.abspath(os.path.join(swd, ".."))
+    swd = os.path.dirname(os.path.abspath(__file__))
+    return os.path.abspath(os.path.join(swd, ".."))
+
 
 def test_lvl0(root_dir):
-  command = "%s/tq . %s/test/simple.toml" % (root_dir, root_dir)
-  result = subprocess.run(command, shell=True, capture_output=True)
-  expected = "{'title': 'TOML Example 2345'}"
-  assert result.stdout.decode().strip() == expected
+    command = "%s/tq . %s/test/simple.toml" % (root_dir, root_dir)
+    result = subprocess.run(command, shell=True, capture_output=True)
+    expected = "{'title': 'TOML Example 2345'}"
+    assert result.stdout.decode().strip() == expected
+
 
 def test_lvl1(root_dir):
-  command = "%s/tq .title %s/test/test.toml" % (root_dir, root_dir)
-  result = subprocess.run(command, shell=True, capture_output=True)
-  expected = "TOML Example"
-  assert result.stdout.decode().strip() == expected
+    command = "%s/tq .title %s/test/test.toml" % (root_dir, root_dir)
+    result = subprocess.run(command, shell=True, capture_output=True)
+    expected = "TOML Example"
+    assert result.stdout.decode().strip() == expected
+
 
 def test_lvl2(root_dir):
-  command = "%s/tq .database.ports %s/test/test.toml" % (root_dir, root_dir)
-  result = subprocess.run(command, shell=True, capture_output=True)
-  expected = '[8001, 8001, 8002]'
-  assert result.stdout.decode().strip() == expected
+    command = "%s/tq .database.ports %s/test/test.toml" % (root_dir, root_dir)
+    result = subprocess.run(command, shell=True, capture_output=True)
+    expected = "[8001, 8001, 8002]"
+    assert result.stdout.decode().strip() == expected
