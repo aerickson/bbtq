078599daadba4828a3574201eaefb4dc9cd9455a
Andrew Erickson <aerickson@mozilla.com>
2020-02-20 11:50:58 -0800

    test_core: use canonical name, fix indents

--- btq/tests/test_core.py
+++ btq/tests/test_core.py
@@ -6,26 +6,30 @@ import toml
 
 
 def test_basic():
-  toml_object = toml.loads("[car]\nname = 'fun car'")
-  result = btq.filter_toml(toml_object, ".car.name")
-  expected = "fun car"
-  assert result == expected
+    toml_object = toml.loads("[car]\nname = 'fun car'")
+    result = btq.filter_toml(toml_object, ".car.name")
+    expected = "fun car"
+    assert result == expected
+
 
 def test_filter_exceptions_1():
-  with pytest.raises(btq.core.BTQInvalidKeyException):
-    toml_object = toml.loads("[car]\nname = 'fun car'")
-    _r = btq.filter_toml(toml_object, ".car.zzz")
+    with pytest.raises(btq.BTQInvalidKeyException):
+        toml_object = toml.loads("[car]\nname = 'fun car'")
+        _r = btq.filter_toml(toml_object, ".car.zzz")
+
 
 def test_filter_exceptions_2():
-  with pytest.raises(btq.core.BTQInvalidIndexException):
-    toml_object = toml.loads("[car]\nname = [1,2,3]")
-    _r = btq.filter_toml(toml_object, ".car.name[7]")
+    with pytest.raises(btq.BTQInvalidIndexException):
+        toml_object = toml.loads("[car]\nname = [1,2,3]")
+        _r = btq.filter_toml(toml_object, ".car.name[7]")
+
 
 def test_filter_exceptions_3():
-  with pytest.raises(btq.core.BTQInvaildArrayFilterException):
-    toml_object = toml.loads("[car]\nname = [1,2,3]")
-    _r = btq.filter_toml(toml_object, ".car.name[string]")
+    with pytest.raises(btq.BTQInvaildArrayFilterException):
+        toml_object = toml.loads("[car]\nname = [1,2,3]")
+        _r = btq.filter_toml(toml_object, ".car.name[string]")
+
 
 def test_main_exception_bad_file():
-  with pytest.raises(FileNotFoundError):
-    btq.main('bad_path', 'test_filter')
+    with pytest.raises(FileNotFoundError):
+        btq.main('bad_path', 'test_filter')
