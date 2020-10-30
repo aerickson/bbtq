c9b0ef61ceb579571b6a2d583ec15d6007a748a6
Andrew Erickson <aerickson@mozilla.com>
2020-10-30 14:35:55 -0700

    version is currently 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='btq',
-    version='0.1.0',
+    version='1.0.0',
     author='Andrew J. Erickson',
     author_email='aerickson@gmail.com',
     packages=['btq'],
