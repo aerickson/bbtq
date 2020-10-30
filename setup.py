fa3d31fcc30d1c332cb8b324f56977b0e955c505
Andrew Erickson <aerickson@mozilla.com>
2020-10-30 16:43:23 -0700

    fix black failure

--- setup.py
+++ setup.py
@@ -11,5 +11,7 @@ setup(
     license="GPLv3",
     description="Useful towel-related stuff.",
     long_description=open("README.md").read(),
-    install_requires=["toml >= 0.10.0",],  # noqa
+    install_requires=[
+        "toml >= 0.10.0",
+    ],
 )
