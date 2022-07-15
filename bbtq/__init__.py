df7861d875a1e35209e35f89bce06f58265bb3d3
Andrew Erickson <aerickson@mozilla.com>
2022-07-14 22:54:33 -0700

    python code gets version from pyproject.toml

--- bbtq/__init__.py
+++ bbtq/__init__.py
@@ -1,8 +1,15 @@
 # __init__.py
 """ __init__.py """
 
-__all__ = ["core"]
-__version__ = "3.0.0"
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata
+
+__all__ = ["__version__", "core"]
+package_name = "bbtq"
+__version__ = metadata.version(package_name)
 
 from .core import (  # noqa
     main,
